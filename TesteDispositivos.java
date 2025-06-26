ad4d0b8233af1af8a27bac65db9608a99947a98c
themanoel7 <thiagoemanoel199524@gmail.com>
2025-06-26 09:53:38 -0300

    primeiro commit

---
+++ TesteDispositivos.java
@@ -0,0 +1,26 @@
+package listaoop6questao1;
+
+public class TesteDispositivos {
+    public static void main(String[] args) {
+        DispositivoEletronico tv = new Televisao("Smart TV Samsung");
+        DispositivoEletronico radio = new Radio("Rádio JBL");
+        DispositivoEletronico notebook = new Notebook("Notebook Dell");
+
+        tv.ligar();
+        tv.exibirStatus();
+        tv.resetar();
+        tv.desligar();
+
+        System.out.println();
+
+        radio.ligar();
+        radio.exibirStatus();
+
+        System.out.println();
+
+        notebook.exibirStatus();
+        notebook.ligar();
+        notebook.resetar();
+    }
+}
+
